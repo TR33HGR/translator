8ea8f06fb46bd13a75c03398a191dd1299e747da
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2024-08-03 19:06:13 +0100

    Play with Flask templates

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,7 +1,10 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index() -> str:
-    return "Hello, World!"
+    user = {'username': 'TR33HGR'}
+    return render_template('index.html', title='Home', user=user)
