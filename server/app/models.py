c0ab8986b6a7225d274c0bed194f563ad07e030e
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2024-08-04 19:20:51 +0100

    Add password hashing

--- server/app/models.py
+++ server/app/models.py
@@ -1,6 +1,7 @@
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from app import db
 
@@ -15,3 +16,9 @@ class User(db.Model):
 
     def __rep__(self) -> str:
         return f'<User {self.username}>'
+
+    def set_password(self, password: str) -> None:
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
