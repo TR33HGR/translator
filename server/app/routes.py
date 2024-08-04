a7de66b2ff9d2f9a21367d89bcb5994e2cbe0459
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2024-08-04 13:52:58 +0100

    Replace direct links with url_for()

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,4 +1,4 @@
-from flask import flash, redirect, render_template
+from flask import flash, redirect, render_template, url_for
 
 from app import app
 from app.forms import LoginForm
@@ -17,5 +17,5 @@ def login() -> str:
     if form.validate_on_submit():
         flash(f'Login request user {form.username.data}, remember me={
               form.remember_me.data}')
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
