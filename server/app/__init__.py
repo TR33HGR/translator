57663235738732e7d3e44299cbc5f1847a950b0a
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2024-08-06 21:16:13 +0100

    WIP: TO TEST: Configure email sending on error

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,3 +1,5 @@
+import logging
+from logging.handlers import SMTPHandler
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -11,6 +13,24 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth: tuple[str, str] = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure: tuple[()] = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr=f'no-reply@{app.config['MAIL_SERVER']}',
+            toaddrs=app.config['ADMINS'],
+            subject='App Failure',
+            credentials=auth,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
 # pylint: disable=C0413, W0406
 from app import routes, models, errors  # noqa: E402, F401
